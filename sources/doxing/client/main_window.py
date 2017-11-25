732420b3f615cf131d5395c8fa869361b909166c
Lukasz Deptula <lukasz.deptula@yahoo.com>
2017-11-25 06:25:28 +0100

    File in top bar menu
    Signed-off-by: Lukasz Deptula <lukasz.deptula@yahoo.com>

--- sources/doxing/client/main_window.py
+++ sources/doxing/client/main_window.py
@@ -1,3 +1,5 @@
+from kivy.uix.button import Button
+from kivy.uix.dropdown import DropDown
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.treeview import TreeView, TreeViewLabel
 
@@ -22,9 +24,40 @@ class FileNavigator(TreeView):
         self.add_node(TreeViewLabel(text="TBD"), parent=remote_documents)
 
 
+class TopMenu(GridLayout):
+    def __init__(self):
+        super(TopMenu, self).__init__(rows=1, size_hint_y=None, height=25)
+
+        self._init_file_menu()
+
+    def _init_file_menu(self):
+        dropdown = DropDown()
+
+        new_file = Button(text='New file', size_hint_y=None, height=25)
+        new_file.bind(on_release=lambda btn: dropdown.select(btn.text))
+        dropdown.add_widget(new_file)
+
+        open_file = Button(text='Open file', size_hint_y=None, height=25)
+        open_file.bind(on_release=lambda btn: dropdown.select(btn.text))
+        dropdown.add_widget(open_file)
+
+        save_file = Button(text='Save file', size_hint_y=None, height=25)
+        save_file.bind(on_release=lambda btn: dropdown.select(btn.text))
+        dropdown.add_widget(save_file)
+
+        file_button = Button(text='File', size_hint_y=None, height=25)
+        file_button.bind(on_release=lambda *a, **k: dropdown.open(*a, **k))
+
+        self.add_widget(file_button)
+
+
 class MainWindow(GridLayout):
     def __init__(self):
-        super(MainWindow, self).__init__(cols=2)
+        super(MainWindow, self).__init__(rows=2)
 
-        self.add_widget(FileNavigator())
-        self.add_widget(FileEditor())
+        self.add_widget(TopMenu())
+
+        content = GridLayout(cols=2)
+        content.add_widget(FileNavigator())
+        content.add_widget(FileEditor())
+        self.add_widget(content)
